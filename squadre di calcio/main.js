ed3e663d2135b9cf4d062ba08635e4522d8dfe4f
Alek90 <4lek.90@gmail.com>
2021-10-25 12:41:48 +0200

    Consegna squadre bis

--- squadre di calcio/main.js
+++ squadre di calcio/main.js
@@ -4,17 +4,17 @@
 
 // setting punteggi
 
-const falliSubiti = 0;
-const punti = 0;
+let falliSubiti = 0;
+let punti = 0;
 
 //Squadre:
- let squadre = [
-     {
-         società: "Inter",
-         falliSubiti,
-         punti
-     },
-     {
+let squadre = [
+    {
+        società: "Inter",
+        falliSubiti,
+        punti
+    },
+    {
         società: "Napoli",
         falliSubiti,
         punti
@@ -31,4 +31,23 @@ const punti = 0;
     }
 ] 
 
+console.log(squadre);
+
+/* Generare numeri random in sostituzione dei valori 0 nelle proprietà falliSubiti e punti.
+*/
+
+// funzione utile:
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+
+for (i=0; i<squadre.length; i++) {
+
+    squadre[i].falliSubiti = getRndInteger(0, 10);
+
+    squadre[i].punti = getRndInteger(0, 15);
+
+}
 console.log(squadre);
